ee07b02cdffb57785ea777d99883aaa5b8af08c6
Randy Gingeleski <rjg26247@gmail.com>
2018-10-03 19:37:59 -0400

    close #3

--- demo.js
+++ demo.js
@@ -1,55 +1,30 @@
-<html>
-    <head><title>Demo</title></head>
-    <body>
-        <form>
-            Number 1:<br>
-            <input type="text" name="num1" class="input"><br>
-            Number 2:<br>
-            <input type="text" name="num2" class="input">
-        </form>
-        <p>Added together they are <span id="result"></span></p>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script src="moment.min.js"></script>
-        <script>
-            $('.input').on('change', function() {
-                var number1 = parseInt($('[name="num1"]').val());
-                var number2 = parseInt($('[name="num2"]').val());
-                var result = number1 + number2;
-                $('#result').text(result);
-            })
-        </script>
-        <form>
-            Start Date:<br>
-            <input type="date" name="start" class="input"><br>
-            End Date:<br>
-            <input type="date" name="end" class="input"/>
-        </form>
-        <p>Length of time between dates is: <span id="days"></span></p>
-        <script src="moment.min.js"></script>
-        <script>
-            moment.fn.diff = function result() {
-            $('.input').on('change', function() {
-                var a = moment.$(start);
-                var b = moment.$(end);
-                var days = a.diff(b, 'days')
-                
-                
-                // var a = document.getElementById("start").value;
-                // var b = document.getElementById("end").value;
-            
-                // var start = moment(a).format('DD-MM-YYYY');
-                // var end = moment(b).format('DD-MM-YYYY');
+$('.input').on('change', function() {
+    var number1 = parseInt($('[name="num1"]').val());
+    var number2 = parseInt($('[name="num2"]').val());
+    var result = number1 + number2;
+    $('#result').text(result);
+})
 
-                // var days = end.diff(start, 'days');
-            }) }
-            // var c = moment.$(a);
-            // var d = moment.$(b);
-            // return result2 = c.diff(d, 'days')+1
-            // }) } 
+moment.fn.diff = function result() {
+$('.input').on('change', function() {
+    var a = moment.$(start);
+    var b = moment.$(end);
+    var days = a.diff(b, 'days')
+    
+    
+    // var a = document.getElementById("start").value;
+    // var b = document.getElementById("end").value;
 
-            // var myElement = document.getElementById("intro");
-            //var diff = (Date.parse(end_date) - Date.parse(start_date)) / 86400000;
-            //the division by 86400000 is because 24 hours * 60 mins * 60 seconds * 1000 milliseconds
-        </script>
-    </body>
-</html>
+    // var start = moment(a).format('DD-MM-YYYY');
+    // var end = moment(b).format('DD-MM-YYYY');
+
+    // var days = end.diff(start, 'days');
+}) }
+// var c = moment.$(a);
+// var d = moment.$(b);
+// return result2 = c.diff(d, 'days')+1
+// }) } 
+
+// var myElement = document.getElementById("intro");
+//var diff = (Date.parse(end_date) - Date.parse(start_date)) / 86400000;
+//the division by 86400000 is because 24 hours * 60 mins * 60 seconds * 1000 milliseconds
